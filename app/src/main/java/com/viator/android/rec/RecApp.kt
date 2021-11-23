12e23a56857fb46d1d70f1ac84d9f557aa7bef63
Ricardo Frade <rfrade@tripadvisor.com>
2021-11-23 11:01:40 +0000

    Add project

---
+++ app/src/main/java/com/viator/android/rec/RecApp.kt
@@ -0,0 +1,5 @@
+package com.viator.android.rec
+
+import android.app.Application
+
+class RecApp : Application()
---
+++ app/src/main/java/com/viator/android/rec/ui/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.viator.android.rec.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel()
