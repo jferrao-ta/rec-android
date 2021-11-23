26211f4b9d238b5cb040fe4155789923b6d452f9
PedroOkawa <pedro.henrique.okawa@gmail.com>
2021-11-23 12:48:24 +0000

    Add proper visibility validation

--- app/src/main/java/com/viator/android/rec/ui/main/MainFragment.kt
+++ app/src/main/java/com/viator/android/rec/ui/main/MainFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.viator.android.rec.R
@@ -15,6 +18,8 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private var btPressHere: Button? = null
+    private var message: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +31,20 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setupViews(view)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java].apply {
+            state.observe(viewLifecycleOwner, ::onStateChanged)
+        }
+    }
+
+    private fun setupViews(view: View) {
+        btPressHere = view.findViewById(R.id.btPressHere)
+        message = view.findViewById(R.id.message)
+
+        btPressHere?.setOnClickListener { viewModel.onButtonClicked() }
+    }
+
+    private fun onStateChanged(state: MainViewModel.State) {
+        message?.isVisible = state.visible
     }
 }
--- app/src/main/java/com/viator/android/rec/ui/main/MainViewModel.kt
+++ app/src/main/java/com/viator/android/rec/ui/main/MainViewModel.kt
@@ -1,5 +1,20 @@
 package com.viator.android.rec.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel()
+class MainViewModel : ViewModel() {
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State> = _state
+
+    fun onButtonClicked() {
+        val newValue = _state.value?.visible ?: false
+        _state.postValue(State(visible = !newValue))
+    }
+
+    data class State(
+        val visible: Boolean
+    )
+}
